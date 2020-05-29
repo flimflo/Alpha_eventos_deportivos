e3c2582e8e0cc1df8255cba11331b1ec0799e7be
Salvador Barboza <salvador.barboza.portillo@gmail.com>
2020-05-28 19:05:10 -0500

    Add search

--- src/components/news-list/news-list.vue
+++ src/components/news-list/news-list.vue
@@ -6,6 +6,23 @@
   margin: auto;
   margin-bottom: 24px;
 }
+.search-bar {
+  max-width: 750px;
+  padding: 12px;
+  font-size: 20px;
+  border: 0;
+  border-radius: 38px;
+  background-color: #f5f5f5;
+  box-shadow: 0px 4px 8px #00000036;
+  font-weight: 200;
+  color: #404040;
+  margin-bottom: 24px;
+}
+
+
+.search-bar:focus{
+    outline:none;
+}
 
 th, td {
   padding: 8px
@@ -34,8 +51,11 @@ span {
 </style>
 <template>
 <div>
+  <div class="search-bar-wrapper">
+    <input v-model="keyword" class="input-group search-bar mx-auto" placeholder="Buscar">
+  </div>
   <div v-if="showError">Ha ocurrido un error al cargar las noticias</div>
-  <div v-for="(article, index) in articles" :key="index" class="card">
+  <div v-for="(article, index) in _filteredArticles" :key="index" class="card">
     <h1>{{ article.title }}</h1>
     <span>{{ article.date }}</span>
     <img v-bind:src="article.image_url">
@@ -55,6 +75,19 @@ export default class extends Vue {
   articles: NewsArticle[] = []
   showError = false
   showLoading = false
+
+  keyword = ""
+
+  get _filteredArticles() {
+    if (this.keyword == "") {
+      return this.articles
+    }
+
+    return this.articles.filter(x =>
+      x.title.concat(x.content)
+      .toLowerCase()
+      .includes(this.keyword.toLowerCase()))
+  }
 
   created() {
     getNews().then(articles => {
